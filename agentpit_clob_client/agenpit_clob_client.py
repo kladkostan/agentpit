6a9614cf8e78a4a3f777c9ac8846b09749f566a3
Konstantin Kladko <13399135+kladkogex@users.noreply.github.com>
2026-02-23 15:55:57 +0000

    feat: integrate plyvel for order serialization in AgentPitClobClient

--- agentpit_clob_client/agenpit_clob_client.py
+++ agentpit_clob_client/agenpit_clob_client.py
@@ -1,13 +1,12 @@
 from py_clob_client import ClobClient, OrderType
 
 import logging
-import json
-import time
-from typing import Optional
-
-from py_builder_signing_sdk.config import BuilderConfig
+import plyvel
 
 from py_clob_client.clob_types import PostOrdersArgs
+from py_clob_client.utilities import order_to_json
+
+
 
 
 class AgentPitClobClient:
@@ -15,14 +14,11 @@ class AgentPitClobClient:
         self
     ):
         self.logger = logging.getLogger(self.__class__.__name__)
-
+        self.db = plyvel.DB('/tmp/x', create_if_missing=True)
 
     def post_order(self, order, orderType: OrderType, post_only: bool):
+        serialized_body = order_to_json(order, self.creds.api_key, orderType, post_only)
         pass
 
     def post_orders(self, args: list[PostOrdersArgs]):
         pass
-
-
-
-
