7f44a37454614ebf16ca1e6909879c4dcb02c496
Konstantin Kladko <13399135+kladkogex@users.noreply.github.com>
2026-02-23 16:34:31 +0000

    feat: switch from plyvel to sqlite3 for order storage in AgentPitClobClient

--- agentpit_clob_client/agenpit_clob_client.py
+++ agentpit_clob_client/agenpit_clob_client.py
@@ -1,7 +1,9 @@
+from py_order_utils.model import Order
+
 from py_clob_client import ClobClient, OrderType
 
 import logging
-import plyvel
+import sqlite3
 
 from py_clob_client.clob_types import PostOrdersArgs
 from py_clob_client.utilities import order_to_json
@@ -11,14 +13,69 @@ from py_clob_client.utilities import order_to_json
 
 class AgentPitClobClient:
     def __init__(
-        self
+        self,
+        api_key: str
     ):
+        self.api_key = api_key
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.db = plyvel.DB('/tmp/x', create_if_missing=True)
+        self.db = sqlite3.connect('/tmp/x.db')
+        self.db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS orders (
+                api_key TEXT,
+                price REAL,
+                post_only INTEGER,
+                order_type TEXT,
+                salt TEXT,
+                maker TEXT,
+                signer TEXT,
+                taker TEXT,
+                tokenId TEXT,
+                makerAmount TEXT,
+                takerAmount TEXT,
+                expiration TEXT,
+                nonce TEXT,
+                feeRateBps TEXT,
+                side TEXT,
+                signatureType TEXT,
+                order_json TEXT
+            )
+            """
+        )
 
-    def post_order(self, order, orderType: OrderType, post_only: bool):
-        serialized_body = order_to_json(order, self.creds.api_key, orderType, post_only)
-        self.db.put(self.creds.api_key.encode(), serialized_body.encode())
+    def post_order(self, order : Order, order_type: OrderType, post_only: bool):
+        serialized_body = order_to_json(order, self.api_key, order_type, post_only)
+        self.db.execute(
+            """
+            INSERT INTO orders (
+                api_key, price, post_only, order_type,
+                salt, maker, signer, taker, tokenId,
+                makerAmount, takerAmount, expiration, nonce,
+                feeRateBps, side, signatureType, order_json
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+            (
+                self.api_key,
+                order.price,
+                int(post_only),
+                str(order_type),
+                order.salt,
+                order.maker,
+                order.signer,
+                order.taker,
+                order.tokenId,
+                order.makerAmount,
+                order.takerAmount,
+                order.expiration,
+                order.nonce,
+                order.feeRateBps,
+                order.side,
+                order.signatureType,
+                serialized_body
+            )
+        )
+        self.db.commit()
 
     def post_orders(self, args: list[PostOrdersArgs]):
-        pass
+        for arg in args:
+            self.post_order(arg.order, arg.order_type, arg.post_only)
