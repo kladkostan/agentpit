bc9130d7536cf7b158ec9d547fe37779ae4570c8
kladkogex <13399135+kladkogex@users.noreply.github.com>
2026-03-09 15:58:52 +0000

    feat: add update_market_outcomes method for updating market states in the database

--- tests/fastapi/test_main.py
+++ tests/fastapi/test_main.py
@@ -3,22 +3,9 @@ Main test file - imports all tests from submodules for backward compatibility.
 This allows running all tests with: pytest tests/fastapi/test_main.py
 """
 
-import pytest
-
 # Import all test functions from submodules
-from .test_basic import *
-from .test_markets import *
 
-from .test_basic import *
-from .test_history import *
-from .test_lifecycle import *
-from .test_markets import *
-from .test_polymarket_sync import *
 from .test_portfolio import *
-from .test_positions import *
-from .test_resolution import *
-from .test_usdc import *
-
 
 if __name__ == "__main__":
     # This allows running all tests by executing this file directly
